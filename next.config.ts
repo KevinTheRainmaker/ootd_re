81a4668f8295fee6870872b57b95063a5e4cab3b
KevinTheRainmaker <sweetrainforyou@gmail.com>
2026-06-01 00:38:36 +0900

    feat: implement bg remove

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.experiments = {
+        ...config.experiments,
+        asyncWebAssembly: true,
+      };
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
